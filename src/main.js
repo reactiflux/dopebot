2acb5521591bd2d14f3da3c9bd7b098017278164
Evert Bouw <bouw@outlook.com>
2019-01-18 14:57:50 +0100

    rewrite in rxjs

--- src/main.js
+++ src/main.js
@@ -1,51 +1,73 @@
 const Discord = require("discord.js");
 const { exec } = require("child_process");
+const { fromEventPattern, of } = require("rxjs");
+const {
+  filter,
+  groupBy,
+  map,
+  mergeMap,
+  share,
+  tap,
+  throttleTime
+} = require("rxjs/operators");
 require("dotenv").config();
 
 const client = new Discord.Client();
-
-client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
-
 const regex = /`{3}js?([\s\S]*)`{3}/;
 
-let lastmessage = 0;
+const message$ = fromEventPattern(handler => {
+  client.on("message", handler);
+}).pipe(share());
 
-client.on("message", msg => {
-  try {
-    if (msg.content.startsWith("?eval")) {
-      const matches = msg.content.match(regex);
-      if (matches == null || matches.length !== 2) return;
-      if ((Date.now() - lastmessage) < (1000 * 30)) return;
-      lastmessage = Date.now();
-      const base64data = Buffer.from(matches[1]).toString("base64");
+message$
+  .pipe(filter(message => /^\?h[ea]lp$/.test(message.content)))
+  .subscribe(message => {
+    message.channel.send(
+      "Need some help evaluating your hacky code? Ooof. Send a message like\n?eval\n\\```js\nput your code here\n\\```"
+    );
+  });
 
-      exec(
-        `deno <( echo -e "['libdeno','deno','compilerMain'].forEach(p=>delete window[p]);console.log(eval(window.atob('${base64data}')))" )`,
-        { timeout: 2000, shell: "/bin/bash" },
-        (error, stdout, stderr) => {
-          console.log(error, stdout, stderr);
-          if (error) {
-            msg.channel.send(
-              error.killed
-                ? "Too long; didn't run"
-                : error.message.split(/\r?\n/)[1]
+message$
+  .pipe(
+    filter(message => /^\?eval/.test(message.content)),
+    groupBy(message => message.author.id),
+    mergeMap(msg$ => msg$.pipe(throttleTime(30 * 1000))),
+    mergeMap(message =>
+      of(message).pipe(
+        map(msg => msg.content.match(regex)),
+        filter(matches => Array.isArray(matches) && matches.length === 2),
+        map(matches => matches[1]),
+        map(code => Buffer.from(code).toString("base64")),
+        mergeMap(encoded =>
+          fromEventPattern(handler => {
+            exec(
+              `deno <( echo -e "['libdeno','deno','compilerMain'].forEach(p=>delete window[p]);console.log(eval(window.atob('${encoded}')))" )`,
+              { timeout: 2000, shell: "/bin/bash" },
+              handler
             );
-          } else {
-            const message = stderr.length ? stderr : stdout;
-            if (message.length > 500) {
-              msg.channel.send("tl;dr");
-            } else {
-              msg.channel.send(message, { code: "ts" });
-            }
-          }
-        }
-      );
+          })
+        ),
+        map(result => result.concat(message))
+      )
+    ),
+    tap(console.log)
+  )
+  .subscribe(
+    ([error, stdout, stderr, message]) => {
+      const res = stderr.length ? stderr : stdout;
+
+      if ((error && error.killed) || res.length > 500) {
+        message.channel.send("tl;dr");
+        return;
+      }
+
+      message.channel.send(error ? error.message.split(/\r?\n/)[1] : res, {
+        code: "js"
+      });
+    },
+    error => {
+      console.log(error);
     }
-  } catch (e) {
-    console.log(e);
-  }
-});
+  );
 
 client.login(process.env.BOT_TOKEN);
