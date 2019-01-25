7917d7e91fbc7f4b6df554b3972421cd251425a9
Evert Bouw <bouw@outlook.com>
2019-01-25 12:13:09 +0100

    add type annotations

--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,58 @@
-const Discord = require("discord.js");
-const { exec } = require("child_process");
-const { fromEventPattern, of } = require("rxjs");
-const {
+import Discord from "discord.js";
+import { exec, ExecException } from "child_process";
+import { fromEventPattern, of, Observable } from "rxjs";
+import {
   filter,
   groupBy,
   map,
   mergeMap,
   share,
   throttleTime,
-  tap,
-  pluck
-} = require("rxjs/operators");
-require("dotenv").config();
+  tap
+} from "rxjs/operators";
+import { config } from "dotenv";
+
+config();
 
 const JS = /`{3}js?([\s\S]*)`{3}/;
 const HELP = /^\?h[ea]lp$/;
 const EVAL = /^\?eval/;
 
-const discordObservable = (discordClient, eventName) =>
-  share()(fromEventPattern(discordClient.on.bind(discordClient, eventName)));
-const throttleKey = (keySelector, duration) => observable =>
-  mergeMap(obs => obs.pipe(throttleTime(duration)))(
-    groupBy(keySelector)(observable)
+interface DiscordObservable {
+  (
+    discordClient: Discord.Client,
+    eventName: "message" | "messageDelete"
+  ): Observable<Discord.Message>;
+  (discordClient: Discord.Client, eventName: "messageUpdate"): Observable<
+    [Discord.Message, Discord.Message]
+  >;
+}
+
+const discordObservable: DiscordObservable = (
+  discordClient: Discord.Client,
+  eventName: string
+): Observable<any> =>
+  fromEventPattern(handler => discordClient.on(eventName, handler)).pipe(
+    share()
+  );
+
+const throttleKey = <Value>(
+  keySelector: (value: Value) => any,
+  duration: number
+) => (observable: Observable<Value>) =>
+  observable.pipe(
+    groupBy(keySelector),
+    mergeMap(obs => obs.pipe(throttleTime(duration)))
   );
 
 const messageOptions = { code: "js" };
 const bashOptions = { timeout: 2000, shell: "/bin/bash" };
-const bashCommand = code =>
+const bashCommand = (code: string) =>
   `deno <( echo -e "['libdeno','deno','compilerMain'].forEach(p=>delete window[p]);console.log(eval(atob('${code}')))" )`;
-const formatResponse = (error, stdout) => {
+const formatResponse = (
+  error: ExecException | null,
+  stdout: string | Buffer
+) => {
   if (error && error.killed) {
     return "That took too long (2s)";
   }
@@ -43,17 +67,35 @@ const formatResponse = (error, stdout) => {
 
   return stdout;
 };
-const doTheThing = message =>
+
+type ExecHandler = (
+  error: ExecException | null,
+  stdout: string | Buffer,
+  stderr: string | Buffer
+) => void;
+type ThingResult = [
+  ExecException | null,
+  string | Buffer,
+  string | Buffer,
+  Discord.Message
+];
+
+const doTheThing = (message: Discord.Message) =>
   of(message).pipe(
     map(msg => msg.content.match(JS)),
-    filter(matches => Array.isArray(matches) && matches.length === 2),
+    filter(
+      (matches): matches is RegExpMatchArray =>
+        Array.isArray(matches) && matches.length === 2
+    ),
     map(matches => matches[1]),
     map(code => Buffer.from(code).toString("base64")),
     map(bashCommand),
     mergeMap(command =>
-      fromEventPattern(exec.bind(undefined, command, bashOptions))
+      fromEventPattern<
+        [ExecException | null, string | Buffer, string | Buffer]
+      >(handler => exec(command, bashOptions, handler as ExecHandler))
     ),
-    map(result => result.concat(message))
+    map(result => [...result, message] as ThingResult)
   );
 
 const client = new Discord.Client();
@@ -62,13 +104,15 @@ const message$ = discordObservable(client, "message");
 const messageUpdate$ = discordObservable(client, "messageUpdate");
 const messageDelete$ = discordObservable(client, "messageDelete");
 
-const resultLog = {};
+const resultLog: { [id: string]: Discord.Message } = {};
 
-const addResultToLog = messageId => res => {
+const addResultToLog = (messageId: Discord.Snowflake) => (
+  res: Discord.Message
+) => {
   resultLog[messageId] = res;
 };
 
-const logToConsole = ([error, stdout, stderr, { content }]) => {
+const logToConsole = ([error, stdout, stderr, { content }]: ThingResult) => {
   console.log({ error, stdout, stderr, content });
 };
 
@@ -88,9 +132,10 @@ message$
     tap(logToConsole)
   )
   .subscribe(([error, stdout, _stderr, message]) => {
-    message.channel
-      .send(formatResponse(error, stdout), messageOptions)
-      .then(addResultToLog(message.id));
+    (message.channel.send(
+      formatResponse(error, stdout),
+      messageOptions
+    ) as Promise<Discord.Message>).then(addResultToLog(message.id));
   });
 
 messageUpdate$
@@ -109,7 +154,7 @@ messageUpdate$
 
 messageDelete$
   .pipe(
-    pluck("id"),
+    map(message => message.id),
     filter(messageId => messageId in resultLog)
   )
   .subscribe(messageId => {
