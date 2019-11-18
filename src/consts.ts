6b11da755057e85f9e2cab3365d38b67cef39a73
Evert Bouw <bouw@outlook.com>
2019-11-18 21:46:39 +0100

    tweak code regex

--- src/consts.ts
+++ src/consts.ts
@@ -1,4 +1,4 @@
-export const JS = /`{3}(t|j)s([\s\S]*)`{3}/;
+export const JS = /`{3}(t|j)s\r?\n([\s\S]*)`{3}/;
 export const HELP = /^\?h[ea]lp$/;
 export const EVAL = /^\?eval/;
 export const DESTRUCT = /^\?SELFDESTRUCT$/;
