5f8b736a818589880f315edf04f669a2fee9de8c
Evert Bouw <bouw@outlook.com>
2019-11-21 20:00:01 +0100

    clear log after 5 mins

--- src/resultLog.ts
+++ src/resultLog.ts
@@ -1,4 +1,5 @@
 import { Message, Snowflake } from "discord.js";
+import { FIVE_MINS } from "./consts";
 
 export type AddResultToLog = (messageId: Snowflake) => (res: Message) => void;
 export type IsInResultLog = (messageId: Snowflake) => boolean;
@@ -7,8 +8,29 @@ export type getFromResultLog = (messageId: Snowflake) => Message;
 
 const resultLog: { [id: string]: Message } = {};
 
+const clearResultLog = () => {
+  for (let key in resultLog) {
+    delete resultLog[key];
+  }
+}
+
+const debounce = (delay: number, fn: () => void) => {
+  let timer: NodeJS.Timeout | undefined;
+  
+  return () => {
+    if (timer) {
+      global.clearTimeout(timer);
+      timer = undefined;
+    }
+    timer = global.setTimeout(fn, delay)
+  }
+}
+
+const clearResultLogDebounced = debounce(FIVE_MINS, clearResultLog);
+
 export const addResultToLog: AddResultToLog = messageId => res => {
   resultLog[messageId] = res;
+  clearResultLogDebounced();
 };
 
 export const isInResultLog: IsInResultLog = messageId => messageId in resultLog;
