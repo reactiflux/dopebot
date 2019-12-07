1b4c39c819010a60b64a15341e8f0efea5a285fb
Evert Bouw <bouw@outlook.com>
2019-12-07 20:43:40 +0100

    refactor fromExec

--- src/observables/__tests__/fromExec.ts
+++ src/observables/__tests__/fromExec.ts
@@ -1,22 +1,22 @@
-import { of } from "rxjs";
-import { mergeMap } from "rxjs/operators";
 import { fromExec } from "../fromExec";
 
 describe("fromExec", () => {
   it("should execute bash commands", async () => {
-    const [, text] = await of("apt help")
-      .pipe(mergeMap(fromExec({ shell: "/bin/bash" })))
-      .toPromise();
+    const [, text] = await fromExec("apt help", {
+      shell: "/bin/bash"
+    }).toPromise();
 
     expect(text).toContain("This APT has Super Cow Powers.");
   });
 
   it("should gracefully report errors", async () => {
-    const [, , output] = await of(
-      "NO_COLOR=true deno <( echo 'console.bog(2 + 2)' ) "
-    )
-      .pipe(mergeMap(fromExec({ shell: "/bin/bash" })))
-      .toPromise();
+    const [
+      ,
+      ,
+      output
+    ] = await fromExec("NO_COLOR=true deno <( echo 'console.bog(2 + 2)' ) ", {
+      shell: "/bin/bash"
+    }).toPromise();
 
     expect(output).toContain(
       "error: Uncaught TypeError: console.bog is not a function"
--- src/observables/fromExec.ts
+++ src/observables/fromExec.ts
@@ -1,7 +1,7 @@
 import { exec, ExecException, ExecOptions } from "child_process";
 import { bindCallback } from "rxjs";
 
-export const fromExec = (execOptions: ExecOptions) => (command: string) =>
+export const fromExec = (command: string, execOptions: ExecOptions) =>
   bindCallback<ExecException | null, string | Buffer, string | Buffer>(
     handler => exec(command, execOptions, handler)
   )();
