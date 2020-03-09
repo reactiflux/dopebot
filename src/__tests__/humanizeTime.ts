c04c2ee04e70932ac6a03ae9d4d88c04eefa4a9d
Evert Bouw <bouw@outlook.com>
2020-03-09 10:57:59 +0100

    do stuff with humanizeTime

--- src/__tests__/humanizeTime.ts
+++ src/__tests__/humanizeTime.ts
@@ -5,3 +5,27 @@ test("humanizeTime", () => {
     "18320 days, 11 hours, 1 minute, 24 seconds"
   );
 });
+
+test("humanizeTime", () => {
+  expect(humanizeTime(-1582887684597)).toBe(
+    "18320 days, 11 hours, 1 minute, 24 seconds"
+  );
+});
+
+test("humanizeTime", () => {
+  expect(humanizeTime(0)).toBe(
+    "0 seconds"
+  );
+});
+
+test("humanizeTime", () => {
+  expect(humanizeTime(Number.POSITIVE_INFINITY)).toBe(
+    "Infinity days"
+  );
+});
+
+test("humanizeTime", () => {
+  expect(humanizeTime(NaN)).toBe(
+    "NaN seconds"
+  );
+});
--- src/humanizeTime.ts
+++ src/humanizeTime.ts
@@ -7,15 +7,34 @@ const words = ["day", "hour", "minute", "second"];
 
 const values = [DAYS, HOURS, MINUTES, SECONDS];
 
+const mod = (a: number, b: number) => a % b;
+
+const flow = (...fns: ((input: any) => any)[]) => (value: any) =>
+  fns.reduce((acc, cur) => cur(acc), value);
+
+const reduce = (fn: (acc: any, cur: any) => any, init: any) => (arr: any[]) =>
+  arr.reduce(fn, init);
+
+const devideBy = (b: number) => (a: number) => a / b;
+
+const slice = (start: number, end: number) => (arr: any[]) =>
+  arr.slice(start, end);
+
 export const humanizeTime = (ms: number) =>
   values
-    .map((value, index, arr) =>
-      Math.floor(
-        arr.slice(0, index).reduce((acc, cur) => acc % cur, ms) / value
-      )
+    .map((value, index, arr): number =>
+      flow(
+        slice(0, index),
+        reduce(mod, Math.abs(ms)),
+        devideBy(value),
+        Math.floor
+      )(arr)
     )
-    .map(
-      (time, index) => time && `${time} ${words[index]}${time === 1 ? "" : "s"}`
-    )
-    .filter(Boolean)
+    .reduce((acc, time, index, arr) => {
+      if (time > 0 || (acc.length === 0 && arr.length === index + 1)) {
+        acc.push(`${time} ${words[index]}${time === 1 ? "" : "s"}`);
+      }
+
+      return acc;
+    }, [] as string[])
     .join(", ");
