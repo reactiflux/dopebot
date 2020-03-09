5c8ee28d1517f75d24b951abf89dbd223d8839fd
Evert Bouw <bouw@outlook.com>
2020-03-09 09:51:23 +0100

    clean up humanizeTime

--- src/humanizeTime.ts
+++ src/humanizeTime.ts
@@ -5,13 +5,15 @@ const DAYS = HOURS * 24;
 
 const words = ["day", "hour", "minute", "second"];
 
+const values = [DAYS, HOURS, MINUTES, SECONDS];
+
 export const humanizeTime = (ms: number) =>
-  [
-    Math.floor(ms / DAYS),
-    Math.floor((ms % DAYS) / HOURS),
-    Math.floor(((ms % DAYS) % HOURS) / MINUTES),
-    Math.floor((((ms % DAYS) % HOURS) % MINUTES) / SECONDS)
-  ]
+  values
+    .map((value, index, arr) =>
+      Math.floor(
+        arr.slice(0, index).reduce((acc, cur) => acc % cur, ms) / value
+      )
+    )
     .map(
       (time, index) => time && `${time} ${words[index]}${time === 1 ? "" : "s"}`
     )
